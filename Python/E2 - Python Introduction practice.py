b294c5b876e5b5cd12fc48d8197c9d34c6890ff8
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2023-03-25 02:37:21 -0500

    Agregando archivos de práctica.

---
+++ Python/E2 - Python Introduction practice.py
@@ -0,0 +1,9 @@
+print("hola mundo")
+
+def suma(numero_a: float, numero_b: float) -> float:
+    """Suma dos números reales ingresados."""
+    return numero_a+numero_b
+
+# print(suma.__doc__)
+
+help(suma)
